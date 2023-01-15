c445e7e15b2679d4a17a73777b379035dec74862
AdeFirdauz <ade.firdaus02@gmail.com>
2023-01-15 11:02:07 +0800

    first commit

---
+++ FDA_E020320030/app/src/main/java/id/ac/poliban/mi/vb/e020320045/fda_e020320045/Activity/IntroActivity.java
@@ -0,0 +1,29 @@
+package id.ac.poliban.mi.vb.e020320045.fda_e020320045.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import id.ac.poliban.mi.vb.e020320045.fda_e020320045.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ConstraintLayout startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
---
+++ FDA_E020320030/app/src/main/java/id/ac/poliban/mi/vb/e020320045/fda_e020320045/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vb.e020320045.fda_e020320045.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
